adec24af16ac8fde76cc8573cf0c06be651c9ede
benjithorpe <benthorpe.dev@gmail.com>
2023-01-22 09:33:37 +0000

    remove config option in adapter, comment out prerender

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      fallback: "200.html",
-    }),
-    prerender: { entries: [] },
+    adapter: adapter(),
   },
 };
 
