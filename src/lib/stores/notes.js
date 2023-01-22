d43923a3332714a253989632673f9825842e2ada
benjithorpe <benthorpe.dev@gmail.com>
2023-01-22 20:43:18 +0000

    fix delete button

--- src/lib/stores/notes.js
+++ src/lib/stores/notes.js
@@ -1,5 +1,4 @@
 import { writable } from "svelte/store";
-// import { nanoid } from "nanoid";
 
 export async function saveNotes(notes) {
   return localStorage.setItem("notes", JSON.stringify(notes));
@@ -10,19 +9,3 @@ function getNotes() {
 }
 
 export const notes = writable(getNotes());
-// const data = [
-//   {
-//     id: nanoid(),
-//     title: "Lorem ipsum dolor sit.",
-//     content: "this is a small note...",
-//     date: new Date(),
-//   },
-//   {
-//     id: nanoid(),
-//     title: "Lorem ipsum dolor sit amet.",
-//     content: "this is a small note...",
-//     date: new Date(),
-//   },
-// ];
-
-// saveNotes(data);
--- src/routes/note/[id]/+page.svelte
+++ src/routes/note/[id]/+page.svelte
@@ -1,12 +1,19 @@
 <script>
-  import dayjs from "dayjs";
   import { page } from "$app/stores";
+  import { goto } from "$app/navigation";
+  import dayjs from "dayjs";
   import { notes, saveNotes } from "../../../lib/stores/notes.js";
 
   const currentNote = $notes.find((note) => note.id === $page.params.id);
 
   let title = currentNote.title;
   let content = currentNote.content;
+
+  function deleteNote() {
+    $notes = $notes.filter((note) => note.id !== $page.params.id);
+    saveNotes($notes); // Save the notes to localStorage
+    goto("/");
+  }
 
   function updateNote() {
     // Update the note title and content
@@ -31,7 +38,11 @@
     <legend class="font-semibold text-xl text-gray-700">Note</legend>
 
     <!-- Delete Note -->
-    <button type="button" class="block text-red-500 ml-auto">
+    <button
+      type="button"
+      on:click={deleteNote}
+      class="block text-red-500 ml-auto"
+    >
       <i class="gg-trash" />
     </button>
     <!-- Update Note -->
