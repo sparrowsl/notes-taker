b4eafda6509c9b8d4184a1e78b2417edb29426f2
benjithorpe <benthorpe.dev@gmail.com>
2023-01-23 13:22:21 +0000

    fix fading bug

--- src/lib/components/NotesCard.svelte
+++ src/lib/components/NotesCard.svelte
@@ -5,7 +5,7 @@
   export let note;
 </script>
 
-<a href="/note/{note.id}" transition:fade>
+<a href="/note/{note.id}" in:fade>
   <div class="bg-[#F7F7F7] rounded-lg shadow-sm p-3">
     {#if note.title}
       <h3 class="font-semibold mb-1 text-gray-800">{note.title}</h3>
